46efe5c3393397aced947728f569a2936b064266
max <maximiliano@woowup.com>
2025-10-31 10:49:03 -0300

    Truncate street field respecting JSON-encoded length limit

--- src/DataQuality/StreetCleanser.php
+++ src/DataQuality/StreetCleanser.php
@@ -8,6 +8,7 @@ class StreetCleanser
 
 	/**
 	 * Truncate street field to maximum allowed length
+	 * Considers JSON serialization length to handle special characters
 	 *
 	 * @param string $street
 	 * @return string
@@ -18,6 +19,51 @@ class StreetCleanser
 			return '';
 		}
 
-		return mb_substr($street, 0, self::MAX_LENGTH);
+		if ($this->isWithinLimit($street)) {
+			return $street;
+		}
+
+		return $this->truncateToFit($street);
+	}
+
+	/**
+	 * Check if string is within JSON length limit
+	 *
+	 * @param string $street
+	 * @return bool
+	 */
+	private function isWithinLimit($street)
+	{
+		return $this->getJsonLength($street) <= self::MAX_LENGTH;
+	}
+
+	/**
+	 * Get JSON encoded length without quotes
+	 *
+	 * @param string $street
+	 * @return int
+	 */
+	private function getJsonLength($street)
+	{
+		return strlen(json_encode($street)) - 2;
+	}
+
+	/**
+	 * Truncate string iteratively until it fits within limit
+	 *
+	 * @param string $street
+	 * @return string
+	 */
+	private function truncateToFit($street)
+	{
+		while (mb_strlen($street) > 0) {
+			$street = mb_substr($street, 0, mb_strlen($street) - 1);
+
+			if ($this->isWithinLimit($street)) {
+				break;
+			}
+		}
+
+		return $street;
 	}
 }
